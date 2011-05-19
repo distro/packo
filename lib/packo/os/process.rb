37a654fa60b573feb1de75cd211095f9193fac4a
shura <shura1991@gmail.com>
2011-05-20 01:14:39 +0200

    add os/process support for linux only

--- lib/packo/os/process.rb
+++ lib/packo/os/process.rb
@@ -17,9 +17,60 @@
 # along with packo. If not, see <http://www.gnu.org/licenses/>.
 #++
 
+require 'packo/system'
+require 'packo/extensions'
+
 module Packo; module OS
 
 class Process
+  include StructLike
+
+  if Packo::System.host.kernel == 'linux'
+    def self.all
+      Dir['/proc/*'].inject([]) {|res, pr|
+        if pr =~ %r{^/proc/(\d+)$}
+          res << Process.new($1, {
+            name: File.read(File.join(pr, 'comm')).strip,
+            command: File.read(File.join(pr, 'cmdline')).strip
+          })
+        else
+          res
+        end
+      }
+    end
+  else
+    fail 'Unsupported platform, contact the developers please.'
+  end
+
+  def self.from_id (id)
+    Process.new(id)
+  end
+
+  def self.kill (what, signal=:INT)
+    if what.is_a?(String) || what.is_a?(Regexp)
+      OS::Process.all.each {|p|
+        p.kill(signal) if p.command.match(what)
+      }
+    else
+      OS::Process.from_id(what).kill(signal)
+    end
+  end
+
+  attr_reader :id
+
+  def initialize (id, data={})
+    @id   = id
+    @data = data
+  end
+
+  def kill (signal=:INT, wait=true)
+    ::Process.kill(signal, @id)
+
+    if wait
+      ::Process.wait(@id)
+    end
+  end
 end
 
 end; end
+
