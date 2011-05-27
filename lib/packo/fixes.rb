864f34a522800880fa01af383e4cfeef8c5e622e
meh <meh@paranoici.org>
2011-05-27 19:14:09 +0200

    Make change for blockenspiel Rubinius only
    Conflicts:
    
    	lib/packo/fixes.rb
    	lib/packo/os.rb

---
+++ lib/packo/fixes.rb
@@ -0,0 +1,28 @@
+#--
+# The license and code on this file belongs and are the one of the projects they're patching
+#++
+
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+  require 'blockenspiel'
+
+  module Blockenspiel
+    module Unmixer
+      def self.unmix(obj_, mod_)  # :nodoc:
+        last_super_ = obj_.singleton_class
+        this_super_ = last_super_.direct_superclass
+        while this_super_
+          if (this_super_ == mod_ || this_super_.respond_to?(:module) && this_super_.module == mod_)
+            _reset_method_cache(obj_)
+            last_super_.superclass = this_super_.direct_superclass
+            _reset_method_cache(obj_)
+            return
+          else
+            last_super_ = this_super_
+            this_super_ = this_super_.direct_superclass
+          end 
+        end 
+        nil 
+      end 
+    end
+  end
+end
