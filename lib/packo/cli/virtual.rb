8bf3db79fd5c84115417fa64db6807664efe7065
meh <meh@paranoici.org>
2011-06-22 13:06:42 +0200

    cli/virtual: lay down basic skeleton

---
+++ lib/packo/cli/virtual.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+#--
+# Copyleft meh. [http://meh.paranoid.pk | meh@paranoici.org]
+#
+# This file is part of packo.
+#
+# packo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# packo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with packo. If not, see <http://www.gnu.org/licenses/>.
+#++
+
+require 'packo/cli/thor'
+
+module Packo; module CLI
+
+class Virtual < Thor
+  include Thor::Actions
+
+  class_option :help, type: :boolean,
+    desc: 'Show help usage'
+end
+
+end; end
